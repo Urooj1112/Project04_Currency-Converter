1400fbbe059d0df8ba44bca22b0bdde6e7759e6b
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2024-06-19 01:01:32 -0700

    Add files via upload

---
+++ Curreny converter/main.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency : any = {
+    USD :1,
+    EUR :0.91,
+    GBP :0.76,
+    INR :74.57,
+    PKR :280,
+};
+let useranswer = await inquirer.prompt([{
+    name :"from",
+    message :"Enter from currency",
+    type :"list",
+    choices :["USD","EUR","GBP","INR","PKR"]
+},
+ {
+    name :"to",
+    message :"Enter to currency",
+    type :"list",
+    choices :["USD","EUR","GBR","INR","PKR"]
+ },
+ {
+    name :"amount",
+    message :"Enter your amount",
+    type :"number"
+ },
+]);
+let fromAmount = currency[useranswer.from]
+let toAmount = currency[useranswer.to]
+let amount = useranswer.amount
+let baseAmount = amount/fromAmount
+let convertedAmount = baseAmount*toAmount
+console.log(`converted Amount ${convertedAmount} `);
